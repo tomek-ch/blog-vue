26ba95ea51d2b14b137fe049909afdad5b39d1f2
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-19 18:42:46 +0200

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <NuxtLink to="/">
+      <h1>
+        Blogg
+      </h1>
+    </NuxtLink>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: { Header }
+};
+</script>
