8cc70f5f7fc883e04d1f79d729d2092ff1ae7d49
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-20 21:05:18 +0200

    Implement post delete

--- components/PostOptions.vue
+++ components/PostOptions.vue
@@ -4,13 +4,25 @@
 
 <script>
 import Options from "./Options";
+import { token } from "@/auth/store";
 
 export default {
   components: { Options },
-  setup() {
+  props: ["id", "handleDelete"],
+  setup(props) {
+    const deletePost = async () => {
+      await fetch(`${process.env.baseUrl}/posts/${props.id}`, {
+        method: "delete",
+        headers: {
+          Authorization: `Bearer ${token.value}`
+        }
+      });
+      props.handleDelete();
+    };
+
     return {
       options: [
-        { text: "Edit", to: `/posts/${_id}/edit` },
+        { text: "Edit", to: `/posts/${props.id}/edit` },
         {
           text: "Delete",
           onClick: deletePost
--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -5,7 +5,10 @@
     </div>
     <div v-else-if="post">
       <h1>{{ post.title }}</h1>
-      <PostDetails :post="post" />
+      <div class="details">
+        <PostDetails :post="post" />
+        <PostOptions :id="post._id" :handleDelete="handleDelete" />
+      </div>
       <div v-for="(p, idx) of post.paragraphs" :key="`p${idx}`">
         <h2>{{ p.heading }}</h2>
         <p>{{ p.body }}</p>
@@ -17,17 +20,28 @@
 </template>
 
 <script>
-import { computed, useRoute, ref, useFetch } from "@nuxtjs/composition-api";
+import {
+  computed,
+  useRoute,
+  ref,
+  useFetch,
+  useRouter
+} from "@nuxtjs/composition-api";
 import Tags from "@/components/Tags";
 import PostDetails from "@/components/PostDetails";
+import PostOptions from "@/components/PostOptions";
 
 export default {
-  components: { Tags, PostDetails },
+  components: { Tags, PostDetails, PostOptions },
   setup() {
     const route = useRoute();
     const id = computed(() => route.value.params.id);
+
     const post = ref(null);
     const comments = ref([]);
+
+    const router = useRouter();
+    const handleDelete = () => router.replace("/");
 
     useFetch(async () => {
       const data = await (
@@ -38,7 +52,14 @@ export default {
       comments.value = data.comments;
     });
 
-    return { post, comments };
+    return { post, comments, handleDelete };
   }
 };
 </script>
+
+<style scoped>
+.details {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
