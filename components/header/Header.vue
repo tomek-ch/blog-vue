cee1db02d39263cd1f8b9a7efaff74edb2052458
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-25 18:16:48 +0200

    Style header

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -3,11 +3,12 @@
     <NuxtLink to="/">
       <h1>
         Blogg
+        <Pen />
       </h1>
     </NuxtLink>
     <UserOptions v-if="user && user._id" />
     <div v-else>
-      <NuxtLink to="/login">
+      <NuxtLink to="/login" class="btn-borderless">
         Log in
       </NuxtLink>
       <NuxtLink to="/register" class="btn-primary">
@@ -20,20 +21,29 @@
 <script>
 import { user, signOut } from "@/auth/store";
 import UserOptions from "./UserOptions";
+import Pen from "@/components/icons/Pen";
 
 export default {
   name: "Header",
-  components: { UserOptions },
+  components: { UserOptions, Pen },
   setup() {
     return { user, signOut };
   }
 };
 </script>
 
-<style>
+<style scoped>
 header {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+h1 {
+  font-size: 1.5em;
+}
+
+.btn-borderless {
+  margin-right: 0.3em;
 }
 </style>
